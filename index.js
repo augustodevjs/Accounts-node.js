58d90f34f269d7015fc2ce1e27f5a34b289c6ca2
João Augusto <augustosaaude@gmail.com>
2022-04-03 14:30:48 -0300

    add inquirer operation

---
+++ index.js
@@ -0,0 +1,24 @@
+// Módulos externos
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+
+// Módulos internos
+const fs = require('fs');
+
+operation();
+
+function operation() {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'action',
+      message: 'O que você deseja fazer?',
+      choices: [ 'Criar conta', 'Consultar Saldo', 'Depositar', 'Sacar', 'Sair']
+    },
+  ])
+  .then((answer) => {
+    const action = answer['action'];
+    console.log(action);
+  })
+  .catch(err => console.log(err))
+};
